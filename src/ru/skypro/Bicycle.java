196c6a8b27c3a5a52d48e146e6704dbfcda762d0
ArtemCH240322 <sun2383@mail.ru>
2022-06-03 08:50:25 +0300

    Первый комит: приступаю. Коды добавил. Переписал. Добавил геттеры и сеттеры. Правкт по замечянию наставника. Получается, что то страшное.

--- src/ru/skypro/Bicycle.java
+++ src/ru/skypro/Bicycle.java
@@ -1,12 +1,24 @@
 package ru.skypro;
 
-public class Bicycle extends WheelableTransport{
+public class Bicycle extends WheelableTransport implements Serviceable, Wheelable {
+
 
     public Bicycle(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
     }
 
+    @Override
     public void updateTyre() {
-        System.out.println("Меняем покрышку");
+        System.out.println("Меняем покрышку велосипеда");
     }
+
+    @Override
+    void check() {
+        System.out.println("Обслуживаем " + getModelName());
+        for (int i = 0; i < getWheelsCount(); i++) {
+            updateTyre();
+        }
+    }
+
+
 }
--- src/ru/skypro/Car.java
+++ src/ru/skypro/Car.java
@@ -1,16 +1,29 @@
 package ru.skypro;
 
-public class Car extends WheelableTransport {
+public class Car extends WheelableTransport implements Serviceable, Wheelable, Engineable{
 
     public Car(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
     }
 
+    @Override
     public void updateTyre() {
-        System.out.println("Меняем покрышку");
+        System.out.println("Меняем покрышку автомобиля");
     }
 
+
+    @Override
     public void checkEngine() {
-        System.out.println("Проверяем двигатель");
+        System.out.println("Проверяем двигатель втомобиля");
+    }
+
+
+    @Override
+    void check() {
+        System.out.println("Обслуживаем " + getModelName());
+        for (int i = 0; i < getWheelsCount(); i++) {
+            updateTyre();
+        }
+        checkEngine();
     }
 }
